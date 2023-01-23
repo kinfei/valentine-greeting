561e8fcc17146f1b70e4d5c6517af0b5b773e093
kinfei <kinfei1230@gmail.com>
2023-01-23 23:02:25 +0800

    Fix typing speed

--- src/components/DisplayName.tsx
+++ src/components/DisplayName.tsx
@@ -3,13 +3,13 @@ import { TypeAnimation } from 'react-type-animation';
 import styles from './DisplayName.less';
 
 function DisplayName({ name }: { name: string | null }): JSX.Element {
-  const sequence = [`Hi ${name}...`, 2400];
+  const sequence = [`Hi ${name}...`, 1800];
 
   if (!name) return <></>;
 
   return (
     <div className={styles.typing}>
-      <TypeAnimation cursor={true} sequence={sequence} speed={70} wrapper='h1' repeat={Infinity} />
+      <TypeAnimation cursor={true} sequence={sequence} speed={10} wrapper='h1' repeat={Infinity} />
     </div>
   );
 }
